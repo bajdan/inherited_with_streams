c93527719aebb3d0e0842ebe952541bbc1f6fa42
bajdan <bhodylogo@gmail.com>
2021-11-04 13:36:44 +0200

    added SinglChildScrollView

--- lib/widgets/student_info.dart
+++ lib/widgets/student_info.dart
@@ -13,57 +13,59 @@ class StudentInfo extends StatefulWidget {
 class _StudentInfoState extends State<StudentInfo> {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: MediaQuery.of(context).size.width * 0.5,
-      child: StreamBuilder<Student>(
-        stream: context
-            .dependOnInheritedWidgetOfExactType<InheritedStreamWidget>()
-            ?.currentStudentController
-            .stream,
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return const Center(
-              child: Text('Nothing selected'),
-            );
-          } else {
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    width: MediaQuery.of(context).size.width * 0.25,
-                    height: MediaQuery.of(context).size.width * 0.4,
-                    decoration: const BoxDecoration(
-                        image: DecorationImage(
-                      image: AssetImage('assets/img/student.png'),
-                      fit: BoxFit.cover,
-                    )),
-                  ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.03,
-                  ),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.4,
-                    child: Text('Name: ${snapshot.data!.name}'),
-                  ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.03,
-                  ),
-                  Text('Surname: ${snapshot.data!.surname}'),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.03,
-                  ),
-                  Text('Age: ${snapshot.data!.age}'),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.03,
-                  ),
-                  Text('Group: ${snapshot.data!.group}'),
-                ],
-              ),
-            );
-          }
-        },
+    return SingleChildScrollView(
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width * 0.5,
+        child: StreamBuilder<Student>(
+          stream: context
+              .dependOnInheritedWidgetOfExactType<InheritedStreamWidget>()
+              ?.currentStudentController
+              .stream,
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(
+                child: Text('Nothing selected'),
+              );
+            } else {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.25,
+                      height: MediaQuery.of(context).size.width * 0.4,
+                      decoration: const BoxDecoration(
+                          image: DecorationImage(
+                        image: AssetImage('assets/img/student.png'),
+                        fit: BoxFit.cover,
+                      )),
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.03,
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.4,
+                      child: Text('Name: ${snapshot.data!.name}'),
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.03,
+                    ),
+                    Text('Surname: ${snapshot.data!.surname}'),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.03,
+                    ),
+                    Text('Age: ${snapshot.data!.age}'),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.03,
+                    ),
+                    Text('Group: ${snapshot.data!.group}'),
+                  ],
+                ),
+              );
+            }
+          },
+        ),
       ),
     );
   }
